e107f5b0f99f8d9d0e0662bd09140d5c5e35cf26
simon <simon@designkojo.net>
2023-08-07 07:06:20 +0900

    add JS script to add class to sibling before elements

--- action.js
+++ action.js
@@ -1,1 +1,31 @@
+const three = document.getElementById('three');
 
+console.log(three);
+
+const siblingBefore = three.previousElementSibling;
+
+console.log('siblingBefore', siblingBefore);
+
+three.addEventListener( 'mouseover', function(){
+    siblingBefore.classList.add('bam');
+});
+three.addEventListener( 'mouseout', function(){
+    siblingBefore.classList.remove('bam');
+});
+
+
+const triggerSiblingBefore = document.querySelectorAll('.inside-main');
+
+console.log(triggerSiblingBefore);
+
+triggerSiblingBefore.forEach((t) => {
+
+    const siblingBefore = t.previousElementSibling;
+
+    t.addEventListener( 'mouseover', function(){
+        siblingBefore.classList.add('baaaaam');
+    });
+    t.addEventListener( 'mouseout', function(){
+        siblingBefore.classList.remove('baaaaam');
+    });
+})
